b87c8bdb1bf802ace92969d66eb186b4ebb093d0
DavidCorrea <898uu1bb11>
2016-08-25 20:49:21 -0300

    Agregado Proyecto GDX.

---
+++ desktop/src/com/magicka/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.magicka.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.magicka.Magicka;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Magicka(), config);
+	}
+}
